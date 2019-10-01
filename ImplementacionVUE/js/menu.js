66822efae24292ba93d69fc475a6da14c2eb7e95
DarAkus1420 <darakus1420@gmail.com>
2019-10-01 17:31:28 -0300

    Avance logeo
    Se implemento vue para logeo y se creo su div

--- ImplementacionVUE/js/menu.js
+++ ImplementacionVUE/js/menu.js
@@ -1,12 +1,12 @@
 const app = new Vue({
 
-   //Se crea el objeto vue que estara implementado en el div principal del index "div menu"
+    //Se crea el objeto vue que estara implementado en el div principal del index "div menu"
     el: "#menu",
     //se colocan los datos que se usaran en este objeto
     data:{
         uploadFileSp: "Subir Archivo",
         uploadFileEn: "Upload File",
         sendSp: "Enviar",
-        sendEn: "Send"
+        sendEn: "Send",
     },
 });
