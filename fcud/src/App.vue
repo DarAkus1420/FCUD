b20537d4a00df38de58772e11f150dff767cbcf4
DarAkus1420 <darakus1420@gmail.com>
2019-10-15 14:49:36 -0300

    App Vue

---
+++ fcud/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <cabecera></cabecera>
+    <lista></lista>
+  </div>
+</template>
+
+<script>
+import Cabecera from './components/Cabecera.vue'
+import Lista from './components/Lista.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Cabecera,
+    Lista
+  }
+}
+</script>
